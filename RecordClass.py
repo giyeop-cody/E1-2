a304df2b9ba92b24094388978af164e34e9117c1
김기엽 <rlduq19938319@c5r2s7.codyssey.kr>
2026-04-01 14:07:31 +0900

    feature: add method object to dict and object from dict

--- RecordClass.py
+++ RecordClass.py
@@ -8,3 +8,20 @@ class Record:
 
     def show_record(self):
         print(f"일시 : {self.timestamp.strftime("%Y년 %M월 %D일")} \t 획득 점수 : {self.total_score} \t 맞춘 문제 : {self.solved_count} \t 푼 문제 : {self.correct_count}")
+
+    def to_dict(self):
+        return {
+            "timestamp": self.timestamp,
+            "total_score": self.total_score,
+            "solved_count": self.solved_count,
+            "correct_count": self.correct_count
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Record(
+            data["timestamp"],
+            data["total_score"],
+            data["solved_count"],
+            data["correct_count"]
+        )
